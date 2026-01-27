a20ca18c82a8bb4d6925802d572a9a7c9a57f60b
Sushant-10-k <sushantjaiswal64@gmail.com>
2026-01-27 20:22:07 +0530

    02 Database:

---
+++ lib/db.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@/lib/generated/prisma";
+const globalforprisma = global as unknown as {
+    prisma: PrismaClient;
+}
+
+const prisma = globalforprisma.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalforprisma.prisma = prisma;
+
+export default prisma;
